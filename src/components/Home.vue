982d33dc54d7dbb791b98991da2fb5c53381d23c
sahil singh <sahilsingh@sahils-Air.gateway.sasktel.net>
2022-10-16 22:25:43 -0600

    initial

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <AddTask @add-task="addTask" />
+  <ItemList
+    :items="items"
+  />
+</template>
+<script>
+import ItemList from '../components/ItemList'
+import AddTask from '../components/AddTask'
+export default {
+  name: 'Home',
+  components: {
+    ItemList,
+    AddTask,
+  },
+  data() {
+    return {
+      items: [],
+    }
+  },
+  methods: {
+     addTask(task) {
+      this.items = [...this.items, task]
+    },
+  },
+   created() {
+    this.items = []
+  },
+}
+</script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,13 @@
+<template>
+    <ui>
+        <div v-for="item in items">
+            <li :key="item.id">{{ item.text }}</li>
+        </div>
+    </ui>
+</template>
+<script>
+export default {
+  name: 'ItemList',
+  props: ['items'],
+}
+</script>
